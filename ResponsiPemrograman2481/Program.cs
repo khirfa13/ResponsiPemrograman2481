ba0fbac28dc807492734cc91d2613022913c65aa
Yura <Yura@KHIRFA>
2022-04-21 10:01:54 +0700

    Add project files.

---
+++ ResponsiPemrograman2481/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2481
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+            Karyawan karyawan3 = new Karyawan();
+
+            Karyawan karyawan1 = new Karyawan(190302123, "Paijo", 3000000);
+            CetakHasil(karyawan1);
+
+            Console.WriteLine();
+
+
+            Karyawan karyawan2 = new Karyawan(190302124, "Jono", 2000000);
+            CetakHasil(karyawan2);
+
+            hasil = karyawan3.Penjumlahan(3000000, 300000);
+            CetakHasil2("Naik gaji: " + hasil);
+
+            Console.ReadKey();
+
+        }
+
+        static void CetakHasil(Karyawan kryn)
+        {
+            Console.WriteLine("Nik: {0}", kryn.Nik);
+            Console.WriteLine("Nama: {0}", kryn.Nama);
+            Console.WriteLine("Gaji Bulanan: {0}", kryn.GajiBulanan);
+        }
+        static void CetakHasil2(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
---
+++ ResponsiPemrograman2481/karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2481
+{
+    class Karyawan
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(int nik, string nama, int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+        }
+
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+    }
+}
